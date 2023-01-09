6a8864b261d1e6f1cf02ee7983710a170bc7be08
Szyres <102583783+Szyres@users.noreply.github.com>
2023-01-09 13:53:24 +0100

    configADC add til gpoi.c og PinA1 og PinA0
    configADC er sat ind plus PinA1 og PinA0 fordi de skal bruges til at læse Analog til Digtal signal fra joystick

--- Src/Spaceship.c
+++ Src/Spaceship.c
@@ -84,54 +84,6 @@ void update_ship_left(spaceship_t *s) {
 	s-> y = s-> y;
 }
 
-void initialize_ports_ship(){
-	RCC->AHBENR |= RCC_AHBPeriph_GPIOA; // Enable Clock GPIO for PORT A
-	//PinA0
-	GPIOA->MODER &= ~(0x00000003 << (0 * 2)); // Clear mode register
-	GPIOA->MODER |= (0x00000000 << (0 * 2));
-	GPIOA->PUPDR &= ~(0x00000003 << (0 * 2)); // Clear push/pull register
-	GPIOA->PUPDR |= (0x00000002 << (0 * 2));
-
-	//PinA1
-	GPIOA->MODER &= ~(0x00000003 << (1 * 2)); // Clear mode register
-	GPIOA->MODER |= (0x00000000 << (1 * 2));
-	GPIOA->PUPDR &= ~(0x00000003 << (1 * 2)); // Clear push/pull register
-	GPIOA->PUPDR |= (0x00000002 << (1 * 2));
-
-	RCC->AHBENR |= RCC_AHBPeriph_GPIOC; // Enable Clock GPIO for PORT C
-
-	GPIOC->MODER &= ~(0x00000003 << (1 * 2)); // Clear mode register
-	GPIOC->MODER |= (0x00000000 << ( 1* 2));
-	GPIOC->PUPDR &= ~(0x00000003 << (1 * 2)); // Clear push/pull register
-	GPIOC->PUPDR |= (0x00000002 << (1* 2));
-
-}
-void configADC() {
-	//Analog-to-Digital Conversion
-	RCC->CFGR2 &= ~RCC_CFGR2_ADCPRE12; // Clear ADC12 prescaler bits
-	RCC->CFGR2 |= RCC_CFGR2_ADCPRE12_DIV6; // Set ADC12 prescaler to 6
-	RCC->AHBENR |= RCC_AHBPeriph_ADC12; // Enable clock for ADC12
-
-	ADC1->CR = 0x00000000; // Clear CR register
-	ADC1->CFGR &= 0xFDFFC007; // Clear ADC1 config register
-	ADC1->SQR1 &= ~ADC_SQR1_L; // Clear regular sequence register 1
-
-	ADC1->CR |= 0x10000000; // Enable internal ADC voltage regulator
-	for (int i = 0 ; i < 1000 ; i++) {} // Wait for about 16 microseconds
-
-	ADC1->CR |= 0x80000000; // Start ADC1 calibration
-	while (!(ADC1->CR & 0x80000000)); // Wait for calibration to finish
-	for (int i = 0 ; i < 100 ; i++) {} // Wait for a little while
-
-	ADC1->CR |= 0x00000001; // Enable ADC1 (0x01 - Enable, 0x02 - Disable)
-	while (!(ADC1->ISR & 0x00000001)); // Wait until ready
-
-	ADC_RegularChannelConfig(ADC1, ADC_Channel_1, 1, ADC_SampleTime_1Cycles5);
-
-	ADC_StartConversion(ADC1); // Start ADC read
-	while (ADC_GetFlagStatus(ADC1, ADC_FLAG_EOC) == 0); // Wait for ADC read
-}
-
 
 int8_t readjoystick(spaceship_t *s){
 
@@ -156,7 +108,7 @@ int main (void) {
 	//drawSpaceship(&s);
 
 	while(1){
-		initialize_ports_ship();
+		init_pins();
 		configADC();
 		readjoystick(&s);
 
