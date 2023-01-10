7ef4240a66a739452d7a0c7aac46bfb8a632a0ca
LouisKamp <louiskampeskildsen99@gmail.com>
2023-01-10 10:46:03 +0100

    test

--- Src/ansi.c
+++ Src/ansi.c
@@ -4,7 +4,7 @@
  *  Created on: 3. jun. 2022
  *      Author: Casper
  */
-
+blasjdbjasljbdaslbjd
 #define ESC 0x1B
 #include "stm32f30x_conf.h" // STM32 config
 #include "30010_io.h" // Input/output library for this course
@@ -12,44 +12,44 @@
 
 void fgcolor(uint8_t foreground) {
 	/*  Value      foreground     Value     foreground
-    ------------------------------------------------
-      0        Black            8       Dark Gray
-      1        Red              9       Light Red
-      2        Green           10       Light Green
-      3        Brown           11       Yellow
-      4        Blue            12       Light Blue
-      5        Purple          13       Light Purple
-      6        Cyan            14       Light Cyan
-      7        Light Gray      15       White
+	------------------------------------------------
+	  0        Black            8       Dark Gray
+	  1        Red              9       Light Red
+	  2        Green           10       Light Green
+	  3        Brown           11       Yellow
+	  4        Blue            12       Light Blue
+	  5        Purple          13       Light Purple
+	  6        Cyan            14       Light Cyan
+	  7        Light Gray      15       White
 	 */
 	uint8_t type = 22;             // normal text
 	if (foreground > 7) {
 		type = 1;                // bold text
 		foreground -= 8;
 	}
-	printf("%c[%d;%dm", ESC, type, foreground+30);
+	printf("%c[%d;%dm", ESC, type, foreground + 30);
 }
 
 void bgcolor(uint8_t background) {
 	/* IMPORTANT:   When you first use this function you cannot get back to true white background in HyperTerminal.
    Why is that? Because ANSI does not support true white background (ANSI white is gray to most human eyes).
-                The designers of HyperTerminal, however, preferred black text on white background, which is why
-                the colors are initially like that, but when the background color is first changed there is no
- 	              way comming back.
+				The designers of HyperTerminal, however, preferred black text on white background, which is why
+				the colors are initially like that, but when the background color is first changed there is no
+				  way comming back.
    Hint:        Use resetbgcolor(); clrscr(); to force HyperTerminal into gray text on black background.
 
-    Value      Color
-    ------------------
-      0        Black
-      1        Red
-      2        Green
-      3        Brown
-      4        Blue
-      5        Purple
-      6        Cyan
-      7        Gray
+	Value      Color
+	------------------
+	  0        Black
+	  1        Red
+	  2        Green
+	  3        Brown
+	  4        Blue
+	  5        Purple
+	  6        Cyan
+	  7        Gray
 	 */
-	printf("%c[%dm", ESC, background+40);
+	printf("%c[%dm", ESC, background + 40);
 }
 
 void color(uint8_t foreground, uint8_t background) {
@@ -59,7 +59,7 @@ void color(uint8_t foreground, uint8_t background) {
 		type = 1;                // bold text
 		foreground -= 8;
 	}
-	printf("%c[%d;%d;%dm", ESC, type, foreground+30, background+40);
+	printf("%c[%d;%d;%dm", ESC, type, foreground + 30, background + 40);
 }
 
 void resetbgcolor() {
@@ -67,31 +67,31 @@ void resetbgcolor() {
 	printf("%c[m", ESC);
 }
 
-void clear(){
-	printf("%c[2J",ESC);
+void clear() {
+	printf("%c[2J", ESC);
 }
-void getxy(int y, int x){
-	printf("%c[%d;%dH",ESC,y,x);
+void getxy(int y, int x) {
+	printf("%c[%d;%dH", ESC, y, x);
 }
-void clreol(){
-	printf("%c[K \n",ESC);
+void clreol() {
+	printf("%c[K \n", ESC);
 }
-void underline(uint8_t on){
-	if (on == 1 ) {
-		printf("%c[4;4m",ESC);
+void underline(uint8_t on) {
+	if (on == 1) {
+		printf("%c[4;4m", ESC);
 	}
 	else {
-		printf("%c[0;0m",ESC);
+		printf("%c[0;0m", ESC);
 	}
 }
-void blink (uint8_t on) {
-	if (on == 1 ) {
-		printf("%c[5;5m",ESC);
+void blink(uint8_t on) {
+	if (on == 1) {
+		printf("%c[5;5m", ESC);
 	}
 	else {
-		printf("%c[0;0m",ESC);
+		printf("%c[0;0m", ESC);
 	}
 }
-void inverse () {
-	printf("%c[7;7m",ESC);
+void inverse() {
+	printf("%c[7;7m", ESC);
 }
