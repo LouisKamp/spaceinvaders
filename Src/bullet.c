b441780b61c4d27f1110fcd9800fd1994b5bcd36
Szyres <102583783+Szyres@users.noreply.github.com>
2023-01-10 17:41:35 +0100

    Update bullet.c

--- Src/bullet.c
+++ Src/bullet.c
@@ -7,19 +7,18 @@
 
 #include "bullet.h"
 
-void initialize_bullet(spaceship_t* s,bullet_t *b) {
+void initialize_bullet(spaceship_t* s,bullet_t *b) { //initialize the bullets coordinates.
 	b->y = s->y+3;
 	b->x = s->x;
 }
 void draw_bullet(joystick_input_t input,bullet_t *b, uint8_t *buffer) { //Draw bullet my press Center on joystick
 	if (input & (1 << 4) ) {
+		set_led(0b100);
 		while (b->y != 122) {
 			lcd_write_char(' ', b->x, b->y, buffer);
 			b->y++;
 			lcd_write_char('-', b->x, b->y, buffer);
-			set_led(0b100);
 		}
-
 	}
 }
 void remove_bullet (spaceship_t* s,bullet_t *b,uint8_t *buffer) { //Removes bullet so it wont stay on wall.
